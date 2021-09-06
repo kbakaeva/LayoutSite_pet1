91f4b5c4c537868d4b876f332c9b5a276f329766
az <az@azs-MacBook-Pro.local>
2021-09-06 14:16:47 +0600

    first commit

---
+++ components/CarsList/cars.js
@@ -0,0 +1,20 @@
+export default [{
+    name: 'Model S',
+    tagline: 'Starting at $69,420',
+    image: require('../../assets/images/ModelS.jpg')
+},{
+    name: 'Model 3',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Model3.jpg')
+},{
+    name: 'Model X',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelX.jpg')
+},{
+    name: 'Model Y',
+    tagline: 'Order Online for',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelY.jpg')
+}]
